b5b3631ee62b8e798e1214f1e0baee4dbbce5479
llongcc <liqin.long@rutgers.edu>
2019-12-09 12:55:32 -0500

    .

--- classification/dataClassifier.py
+++ classification/dataClassifier.py
@@ -409,6 +409,7 @@ def selfRunClassifier():
 
 
     # NaiveBayes part
+<<<<<<< HEAD
     # print "Training by using NaiveBayes Algorithm"
     # featureFunction = enhancedFeatureExtractorDigit
     # legalLabels = range(10)
@@ -447,6 +448,53 @@ def selfRunClassifier():
 
     # Perceptron algorithm
     # print "Training by using Perceptron Algorithm"
+=======
+    print "Training by using NaiveBayes Algorithm"
+    featureFunction = enhancedFeatureExtractorDigit
+    legalLabels = range(10)
+    classifier = naiveBayes.NaiveBayesClassifier(legalLabels)
+    lst_avg_time = []
+    lst_avg_acc = []
+    lst_std_acc = []
+    for percent in data_percent:
+        print "training set size:\t" + str(percent)+"%"
+        # print "setSmoothing: k value is ", classifier.k
+        lst_time = []
+        lst_acc = []
+        for i in range(5):
+            start_time = timeit.default_timer()
+            rawTrainingData = samples.loadDataFile("digitdata/trainingimages", percent,DIGIT_DATUM_WIDTH,DIGIT_DATUM_HEIGHT)
+            trainingLabels = samples.loadLabelsFile("digitdata/traininglabels", percent)
+            rawValidationData = samples.loadDataFile("digitdata/validationimages", TEST_SET_SIZE,DIGIT_DATUM_WIDTH,DIGIT_DATUM_HEIGHT)
+            validationLabels = samples.loadLabelsFile("digitdata/validationlabels", TEST_SET_SIZE)
+            rawTestData = samples.loadDataFile("digitdata/testimages", TEST_SET_SIZE,DIGIT_DATUM_WIDTH,DIGIT_DATUM_HEIGHT)
+            testLabels = samples.loadLabelsFile("digitdata/testlabels", TEST_SET_SIZE)
+            # print "Extracting features..."
+            trainingData = map(featureFunction, rawTrainingData)
+            validationData = map(featureFunction, rawValidationData)
+            testData = map(featureFunction, rawTestData)
+            classifier.train(trainingData, trainingLabels, validationData, validationLabels)
+            elapse = timeit.default_timer() - start_time
+            lst_time.append(elapse)
+            guesses = classifier.classify(testData)
+            correct = [guesses[i] == testLabels[i] for i in range(len(testLabels))].count(True)
+            lst_acc.append(float(correct) / len(testLabels))
+        print '--------------------------------------------------------------'
+        lst_avg_time.append(np.mean(lst_time))
+        lst_avg_acc.append(np.mean(lst_acc))
+        lst_std_acc.append(np.std(lst_acc))
+    analysis(lst_avg_time, lst_avg_acc, lst_std_acc)
+    # Percentron algorithm
+
+
+
+
+
+
+    # K nearest neighbour algorithm
+    print "Training by using KNN Algorithm"
+    print "Only use 10% test set"
+>>>>>>> parent of 7be1fe2... Merge branch 'master' of https://github.com/llongc/Face_Digit_Recognition
     # featureFunction = enhancedFeatureExtractorDigit
     # legalLabels = range(10)
     # classifier = perceptron.PerceptronClassifier(legalLabels, 3)
